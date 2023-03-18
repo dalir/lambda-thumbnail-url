e5c09e9308f96a7294defac29b24f4fe73144ff9
AJ <ajabbari@skycatch.com>
2023-03-18 14:46:18 +0000

    updating variables and adding missing variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,31 +1,28 @@
-# Accounts
-variable "environment" {}
-variable "account_id" {}
-variable "region" {}
-variable "provider_bucket" {}
-variable "provider_role" {}
+variable "account_id" {
+  description = "AWS account ID"
+}
 
-# Versioning
+variable "region" {
+  description = "AWS region to host your network"
+  default = "us-west-2"
+}
+
+variable "lambda_package" {
+  default = "../deployment/package.zip"
+}
+
+variable "environment" {}
 variable "repo_organization" {}
+variable "repo_name" {}
 variable "repo_full_name" {}
 variable "git_sha" {}
 
-# Lambda Configurations
+variable "provider_role" {}
+variable "provider_bucket" {}
 variable "function_name" {}
-variable "lambda_package" {}
-
-# Other
-variable "cloudwatch_logs_retention_days" {}
-variable "debug" {
-  default = "@skycatch/*"
-}
 
 variable "publish" {
   default = false
-}
-
-variable "datadog_log_forwarder_name" {
-  default = ""
 }
 
 # Application Variables
